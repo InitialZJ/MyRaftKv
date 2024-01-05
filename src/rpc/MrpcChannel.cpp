f4b91aaa74f878c8a27192414f44973033e7ad72
InitialZJ <blueskyzzz0418@gmail.com>
2024-01-05 17:01:54 +0800

    完善MrpcChannel类

--- src/rpc/MrpcChannel.cpp
+++ src/rpc/MrpcChannel.cpp
@@ -4,20 +4,34 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
+#include <cerrno>
 #include <string>
 
-#include "rpc_header.pb.h"
-#include <unistd.h>
-#include <cerrno>
 #include "MrpcController.h"
-#include "u"
+#include "rpc_header.pb.h"
+#include "util.h"
 
 void MrpcChannel::CallMethod(const google::protobuf::MethodDescriptor* method,
                              google::protobuf::RpcController* controller,
                              const google::protobuf::Message* request,
                              google::protobuf::Message* response,
                              google::protobuf::Closure* done) {
+  if (m_clientFd == -1) {
+    std::string errMsg;
+    bool rt = newConnect(m_ip.c_str(), m_port, &errMsg);
+    if (!rt) {
+      DPrintf("[func-MrpcChannel::CallMethod]重链接ip: {%s} port: {%d}失败",
+              m_ip.c_str(), m_port);
+      controller->SetFailed(errMsg);
+      return;
+    } else {
+      DPrintf("[func-MrpcChannel::CallMethod]链接ip: {%s} port: {%d}成功",
+              m_ip.c_str(), m_port);
+    }
+  }
+
   // 需要得到序列化后的字节流
   // 4个字节代表rpc_header长度
   src::RpcHeader header;
@@ -26,67 +40,109 @@ void MrpcChannel::CallMethod(const google::protobuf::MethodDescriptor* method,
   std::string service_name = service->name();
   std::string method_name = method->name();
   // args序列化，并得到长度
+  uint32_t args_len = 0;
   std::string args_str;
-  request->SerializeToString(&args_str);
-  uint32_t args_len = args_str.size();
+  if (request->SerializeToString(&args_str)) {
+    args_len = args_str.size();
+  } else {
+    controller->SetFailed("serialize request error!");
+    return;
+  }
+
   header.set_args_len(args_len);
   header.set_method_name(method_name);
   header.set_service_name(service_name);
   // rpc_header序列化
   std::string rpc_header_str;
-  header.SerializeToString(&rpc_header_str);
+  uint32_t rpc_header_len = 0;
+  if (header.SerializeToString(&rpc_header_str)) {
+    rpc_header_len = rpc_header_str.size();
+  } else {
+    controller->SetFailed("serialize rpc header error!");
+  }
   std::string rpc_header_len_str;
-  uint32_t rpc_header_len = rpc_header_str.size();
   rpc_header_len_str.insert(0, std::string((char*)&rpc_header_len, 4));
 
   // 字符串都已经准备好
   std::string send_str{rpc_header_len_str + rpc_header_str + args_str};
 
-  std::string zoo_ip = MrpcApplication::getConfig().Load("zookeeperip");
-  uint16_t zoo_port = stoi(MrpcApplication::getConfig().Load("zookeeperport"));
-
-  std::string zoo_str = "/" + service_name + "/" + method_name;
-  Zookeeperutil zk = Zookeeperutil();
-  zk.start();
-  std::string ip_port = zk.getData(zoo_str);
-  if (ip_port.size() == 0) {
-    std::cout << "未找到zookeeper" << std::endl;
-    exit(EXIT_FAILURE);
+  // 发送rpc请求
+  // 失败会重新链接再发送，重新连接失败会直接return
+  while (-1 == send(m_clientFd, send_str.c_str(), send_str.size(), 0)) {
+    char errtxt[512] = {0};
+    sprintf(errtxt, "send error! errno: %d", errno);
+    std::cout << "尝试重新连接对方ip: " << m_ip << ", 端口: " << m_port
+              << std::endl;
+    close(m_clientFd);
+    m_clientFd = -1;
+    std::string errMsg;
+    bool rt = newConnect(m_ip.c_str(), m_port, &errMsg);
+    if (!rt) {
+      controller->SetFailed(errMsg);
+      return;
+    }
   }
 
-  int idx = ip_port.find_first_of(':');
-  std::string ip = ip_port.substr(0, idx);
-  uint16_t port = std::stoi(ip_port.substr(idx + 1, ip_port.size() - 1 - idx));
-  std::cout << "得到rpc服务ip: " << ip << std::endl;
-  std::cout << "得到rpc服务端口: " << port << std::endl;
-
-  int client_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (client_sock == -1) {
-    controller->SetFailed("client_sock申请失败");
-    return;
-  }
-  struct sockaddr_in sock_addr;
-  sock_addr.sin_family = AF_INET;
-  sock_addr.sin_port = htons(port);
-  sock_addr.sin_addr.s_addr = inet_addr(ip.c_str());
-  if (connect(client_sock, (sockaddr*)&sock_addr, sizeof(sock_addr)) < 0) {
-    controller->SetFailed("连接失败");
-    return;
-  }
-
-  // 发送信息
-  int len = 0;
-  if ((len = send(client_sock, send_str.c_str(), send_str.size(), 0)) < 0) {
-    controller->SetFailed("发送失败");
-    return;
-  }
-
+  // 接收响应
   char buf[1024] = {0};
-  int len2 = 0;
-  if ((len2 = recv(client_sock, buf, 1024, 0)) < 0) {
-    controller->SetFailed("接收失败");
+  int recv_size = 0;
+  if (-1 == (recv_size = recv(m_clientFd, buf, 1024, 0))) {
+    close(m_clientFd);
+    m_clientFd = -1;
+    char errtxt[512] = {0};
+    sprintf(errtxt, "recv error! errno: %d", errno);
+    controller->SetFailed(errtxt);
     return;
   }
-  std::string response_str = buf;
-  response->ParseFromArray(buf, len2);
+
+  // 反序列化
+  if (!response->ParseFromArray(buf, recv_size)) {
+    char errtxt[1050] = {0};
+    sprintf(errtxt, "parse error! response_str: %s", buf);
+    controller->SetFailed(errtxt);
+    return;
+  }
+}
+
+bool MrpcChannel::newConnect(const char* ip, uint16_t port,
+                             std::string* errMsg) {
+  int clientfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == clientfd) {
+    char errtxt[512] = {0};
+    sprintf(errtxt, "recv error! errno: %d", errno);
+    m_clientFd = -1;
+    *errMsg = errtxt;
+    return;
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+  // 连接rpc服务节点
+  if (-1 ==
+      connect(clientfd, (struct sockaddr*)&server_addr, sizeof(server_addr))) {
+    close(clientfd);
+    char errtxt[512] = {0};
+    sprintf(errtxt, "connect fail! errno: %d", errno);
+    m_clientFd = -1;
+    *errMsg = errtxt;
+    return false;
+  }
+  m_clientFd = clientfd;
+  return true;
+}
+
+MrpcChannel::MrpcChannel(std::string ip, short port, bool connetNow)
+    : m_ip(ip), m_port(port), m_clientFd(-1) {
+  if (!connetNow) {
+    return;
+  }
+  std::string errMsg;
+  auto rt = newConnect(ip.c_str(), port, &errMsg);
+  int tryCount = 3;
+  while (!rt && tryCount--) {
+    std::cout << errMsg << std::endl;
+    rt = newConnect(ip.c_str(), port, &errMsg);
+  }
 }
