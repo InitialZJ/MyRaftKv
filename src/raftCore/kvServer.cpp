962131d61e61a5e133674867f01ef13c8450a4e7
InitialZJ <blueskyzzz0418@gmail.com>
2024-01-18 13:09:37 +0800

    继续完善KvServer类

--- src/raftCore/kvServer.cpp
+++ src/raftCore/kvServer.cpp
@@ -73,20 +73,129 @@ void KvServer::Get(const raftKVRpcProctoc::GetArgs* args,
 
     if (ifRequestDuplicate(op.ClientId, op.RequestId) && isLeader) {
       // 如果超时，代表raft集群不保证已经commmit该日志，但是如果是已经提交过的get请求，是可以再执行的，不会违反线性一致性
-			std::string value;
-			bool exist = false;
-			// TODO: 这个执行怎么跟上面的不一样
-			ExecuteGetOpOnKVDB(op, &value, &exist);
-			if (exist) {
-				reply->set_err(OK);
-				reply->set_value(value);
-			} else {
-				reply->set_err(ErrNoKey);
-				reply->set_value("");
-			}
+      std::string value;
+      bool exist = false;
+      // TODO: 这个执行怎么跟上面的不一样
+      ExecuteGetOpOnKVDB(op, &value, &exist);
+      if (exist) {
+        reply->set_err(OK);
+        reply->set_value(value);
+      } else {
+        reply->set_err(ErrNoKey);
+        reply->set_value("");
+      }
     } else {
-			// 让clerk换一个节点重试
-			reply->set_err(ErrWrongLeader);
-		}
+      // 让clerk换一个节点重试
+      reply->set_err(ErrWrongLeader);
+    }
+  } else {
+    // raft已经提交了该command，可以正式开始执行了
+    if (raftCommitOp.ClientId == op.ClientId &&
+        raftCommitOp.RequestId == op.RequestId) {
+      std::string value;
+      bool exist = false;
+      ExecuteGetOpOnKVDB(op, &value, &exist);
+      if (exist) {
+        reply->set_err(OK);
+        reply->set_value(value);
+      } else {
+        reply->set_err(ErrNoKey);
+        reply->set_value("");
+      }
+    } else {
+      reply->set_err(ErrWrongLeader);
+    }
+  }
+  m_mtx.lock();
+  auto tmp = waitApplyCh[raftIndex];
+  waitApplyCh.erase(raftIndex);
+  delete tmp;
+  m_mtx.unlock();
+}
+
+// TODO: 这个函数是干嘛的
+void KvServer::GetCommandFromRaft(ApplyMsg message) {
+  Op op;
+  op.parseFromString(message.Command);
+  DPrintf(
+      "[KvServer::GetCommandFromRaft-kvserver{%d}] , Got Command --> "
+      "Index:{%d} , ClientId {%s}, RequestId {%d}, Opreation {%s}, Key :{%s}, "
+      "Value :{%s}",
+      m_me, message.CommandIndex, &op.ClientId, op.RequestId, &op.Operation,
+      &op.Key, &op.Value);
+  if (message.CommandIndex <= m_lastSnapShotRaftLogIndex) {
+    return;
+  }
+
+  if (!ifRequestDuplicate(op.ClientId, op.RequestId)) {
+    if (op.Operation == "Put") {
+      ExecutePutOpOnKVDB(op);
+    }
+    if (op.Operation == "Append") {
+      ExecuteAppendOpOnKVDB(op);
+    }
+  }
+  // 到这里kvDB已经制作了快照
+  if (m_maxRaftState != -1) {
+    IfNeedToSendSnapShotCommand(message.CommandIndex, 9);
+  }
+  SendMessageToWaitChan(op, message.CommandIndex);
+}
+
+bool KvServer::ifRequestDuplicate(std::string ClientId, int RequestId) {
+  std::lock_guard<std::mutex> lg(m_mtx);
+  if (m_lastRequestId.find(ClientId) == m_lastRequestId.end()) {
+    return false;
+  }
+  return RequestId <= m_lastRequestId[ClientId];
+}
+
+void KvServer::PutAppend(const raftKVRpcProctoc::PutAppendArgs* args,
+                         raftKVRpcProctoc::PutAppendReply* reply) {
+  Op op;
+  op.Operation = args->op();
+  op.Key = args->key();
+  op.Value = args->value();
+  op.ClientId = args->clientid();
+  op.RequestId = args->requestid();
+  int raftIndex = -1;
+  int _ = -1;
+  bool isLeader = false;
+
+  m_raftNode->Start(op, &raftIndex, &_, &isLeader);
+  if (!isLeader) {
+    DPrintf(
+        "[func -KvServer::PutAppend -kvserver{%d}]From Client %s (Request %d) "
+        "To Server %d, key %s, raftIndex %d , but not leader",
+        m_me, &args->clientid(), args->requestid(), m_me, &op.Key, raftIndex);
+
+    reply->set_err(ErrWrongLeader);
+    return;
+  }
+  DPrintf(
+      "[func -KvServer::PutAppend -kvserver{%d}]From Client %s (Request %d) To "
+      "Server %d, key %s, raftIndex %d , is leader ",
+      m_me, &args->clientid(), args->requestid(), m_me, &op.Key, raftIndex);
+  m_mtx.lock();
+  if (waitApplyCh.find(raftIndex) == waitApplyCh.end()) {
+    waitApplyCh.insert(std::make_pair(raftIndex, new LockQueue<Op>()));
+  }
+  auto chForRaftIndex = waitApplyCh[raftIndex];
+  m_mtx.unlock();
+
+  Op raftCommitOp;
+  if (!chForRaftIndex->timeOutPop(CONSENSUS_TIMEOUT, &raftCommitOp)) {
+    DPrintf(
+        "[func -KvServer::PutAppend -kvserver{%d}]TIMEOUT PUTAPPEND !!!! "
+        "Server %d , get Command <-- Index:%d , ClientId %s, RequestId %s, "
+        "Opreation %s Key :%s, Value :%s",
+        m_me, m_me, raftIndex, &op.ClientId, op.RequestId, &op.Operation,
+        &op.Key, &op.Value);
+    if (ifRequestDuplicate(op.ClientId, op.RequestId)) {
+      // TODO: 为什么会重复
+      // 超时了，但是是重复的请求，返回ok
+      // 实际上就算没有超时，在真正执行的时候也要判断是否重复
+      reply->set_err(OK);
+    }
   }
 }
